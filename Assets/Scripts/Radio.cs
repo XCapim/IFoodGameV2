54487279bc632e3ed984df68ae2de9cd0ac90476
XCapim <xcapim92@hotmail.com>
2019-12-04 18:38:30 -0300

    radio Ok

--- Assets/Scripts/Radio.cs
+++ Assets/Scripts/Radio.cs
@@ -6,8 +6,7 @@ public class Radio : MonoBehaviour
 {
     public static Radio instancia=null;
     public AudioSource audioS;
-    public AudioClip Rap,Seum,cap4,vinil,toda,kille,hear,Datena;
-    public float NumeroMusicas;
+    public AudioClip[] Musicas; 
     public bool PausePlay;
 
 
@@ -20,22 +19,22 @@ public class Radio : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioS = GetComponent<AudioSource>();
         PausePlay = true;
-        NumeroMusicas = 0;
+  
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if(!audioS.isPlaying)
+        
+        if (!audioS.isPlaying)
         {
             if (PausePlay)
             {
                 audioS.Play();
             }
-            
+
         }
         else
         {
@@ -43,65 +42,21 @@ public class Radio : MonoBehaviour
             {
                 audioS.Pause();
             }
-            if (NumeroMusicas == 0)
-            {
-             
-                 audioS.clip = Seum;              
-            }
-
-            else if (NumeroMusicas == 1)
-            {
-                audioS.clip = Rap;
-            }
-
-            else if (NumeroMusicas == 2)
-            {
-                audioS.clip = cap4;
-            }
-            else if (NumeroMusicas == 3)
-            {
-                audioS.clip = vinil;
-            }
-            else if (NumeroMusicas == 4)
-            {
-                audioS.clip = toda;
-            }
-            else if (NumeroMusicas == 5)
-            {
-                audioS.clip = kille;
-            }
-            else if (NumeroMusicas == 6)
-            {
-                audioS.clip = hear;
-            }
-
-            else if (NumeroMusicas == 7)
-            {
-                audioS.clip = Datena;
-            }
         }
-
-        if (NumeroMusicas < 0)
-        {
-            NumeroMusicas = 0;
-        }
-        else if (NumeroMusicas > 7)
-        {
-            NumeroMusicas = 7;
-        }
-
 
     }
 
      public void AvancaMusica()
      {
-        NumeroMusicas++;
+
+      audioS.clip= Musicas[Random.Range(0, 7)];
      }
 
     public void VoltaMusica()
      {
-        NumeroMusicas--;
-     }
+     
+        audioS.clip = Musicas[Random.Range(0, 7)];
+    }
 
     public void PauseAndPlay()
     {
