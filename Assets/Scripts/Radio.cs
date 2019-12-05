79bb1590a7566f900ad525c97881060a382dcc49
XCapim <xcapim92@hotmail.com>
2019-12-05 14:20:32 -0300

    hahahahahahahahaha

--- Assets/Scripts/Radio.cs
+++ Assets/Scripts/Radio.cs
@@ -24,7 +24,7 @@ public class Radio : MonoBehaviour
     {
         audioS = GetComponent<AudioSource>();
         PausePlay = true;
-        audioS.clip = Musicas[3];
+        audioS.clip = Musicas[4];
         anima = false;
         
     }
